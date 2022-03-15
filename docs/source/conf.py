82c8d46ad86365462e55410132e0b0868c0a7321
Tyler Barkin <tybarkin@gmail.com>
2022-03-15 16:41:12 -0400

    Trying some bigger document changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Fish Tank'
+copyright = '2022, Barkin'
+author = 'Tyler Barkin'
 
 release = '0.1'
 version = '0.1.0'
